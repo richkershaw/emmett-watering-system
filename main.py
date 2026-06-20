27d046e706c7966d524767c5fb321750ccc99005
Rich K. <me@richkershaw.com>
2026-06-20 16:08:17 +0100

    Switched to an LCD screen and added pixel graphics

--- main.py
+++ main.py
@@ -1,27 +1,102 @@
+wetness = 0
 dryness = 0
 sensor_reading = 0
-OLED.init(128, 64)
+makerbit.connect_lcd(39)
 wet = 300
 dry = 700
-dryness_trigger_level = 80
+dryness_trigger_level = 20
+makerbit.lcd_make_character(LcdChar.C1,
+    makerbit.lcd_character_pixels("""
+        . # # # #
+        # # # # #
+        . . # # .
+        . . # # .
+        . . # # .
+        # # # # #
+        # # # # #
+        . # # # #
+        """))
+makerbit.lcd_make_character(LcdChar.C2,
+    makerbit.lcd_character_pixels("""
+        . . . . .
+        # . . . #
+        . . . # .
+        # . # . .
+        . # . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        """))
+makerbit.lcd_make_character(LcdChar.C3,
+    makerbit.lcd_character_pixels("""
+        . . . . .
+        . . . # #
+        . # # # #
+        # # # # #
+        . # # # #
+        . . # . #
+        . # . # .
+        # . # . #
+        """))
+makerbit.lcd_make_character(LcdChar.C4,
+    makerbit.lcd_character_pixels("""
+        . . . . .
+        # # . . .
+        # # # # .
+        # # # # #
+        # # # # .
+        . # . . .
+        # . . . .
+        . . . . .
+        """))
+makerbit.lcd_make_character(LcdChar.C5,
+    makerbit.lcd_character_pixels("""
+    . . . . .
+# . . . .
+# # . . .
+# # . . .
+# # # . .
+# # # . .
+# # . . .
+# . . . .
+        """))
+makerbit.lcd_make_character(LcdChar.C6,
+    makerbit.lcd_character_pixels("""
+        . . . . .
+        # . . . .
+        # # . . .
+        # # . . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # . . . .
+        """))
 
 def on_every_interval():
-    global sensor_reading, dryness
+    global sensor_reading, dryness, wetness
     sensor_reading = pins.analog_read_pin(AnalogPin.P0)
-    dryness = Math.map(sensor_reading, wet, dry, 0, 100)
-    OLED.clear()
-    OLED.write_string_new_line("Emmett's watering kit")
-    OLED.write_string("Dryness: ")
-    OLED.write_num(dryness)
-    OLED.write_string_new_line(" / 100")
-    if dryness >= dryness_trigger_level:
-        OLED.write_string_new_line("Watering...")
+    dryness = Math.map(sensor_reading, wet, dry, 1, 99)
+    wetness = 100 - Math.round(dryness)
+    makerbit.clear_lcd1602()
+    makerbit.show_string_on_lcd1602("Emmett's Waterer",
+        makerbit.position1602(LcdPosition1602.POS1),
+        16)
+    makerbit.show_string_on_lcd1602("% wet", makerbit.position1602(LcdPosition1602.POS19), 6)
+    makerbit.show_string_on_lcd1602(convert_to_text(wetness),
+        makerbit.position1602(LcdPosition1602.POS17),
+        2)
+    if wetness <= dryness_trigger_level:
+        makerbit.show_string_on_lcd1602("Watering", makerbit.position1602(LcdPosition1602.POS26), 8)
+        makerbit.lcd_show_character1602(LcdChar.C3, makerbit.position1602(LcdPosition1602.POS31))
+        makerbit.lcd_show_character1602(LcdChar.C4, makerbit.position1602(LcdPosition1602.POS32))
         pins.digital_write_pin(DigitalPin.P8, 1)
         basic.pause(10000)
         pins.digital_write_pin(DigitalPin.P8, 0)
-        OLED.write_string_new_line("Waiting to soak...")
+        makerbit.show_string_on_lcd1602("Soaking ", makerbit.position1602(LcdPosition1602.POS26), 8)
         basic.pause(10000)
     else:
         pins.digital_write_pin(DigitalPin.P8, 0)
-        OLED.write_string_new_line("Soil is damp and healthy!")
+        makerbit.show_string_on_lcd1602("Good    ", makerbit.position1602(LcdPosition1602.POS26), 8)
+        makerbit.lcd_show_character1602(LcdChar.C1, makerbit.position1602(LcdPosition1602.POS31))
+        makerbit.lcd_show_character1602(LcdChar.C2, makerbit.position1602(LcdPosition1602.POS32))
 loops.every_interval(3000, on_every_interval)
--- main.ts
+++ main.ts
@@ -1,28 +1,94 @@
+let wetness = 0
 let dryness = 0
 let sensor_reading = 0
-OLED.init(128, 64)
+makerbit.connectLcd(39)
 let wet = 300
 let dry = 700
-let dryness_trigger_level = 80
-loops.everyInterval(3000, function on_every_interval() {
-    
+let dryness_trigger_level = 20
+makerbit.lcdMakeCharacter(LcdChar.c1, makerbit.lcdCharacterPixels(`
+    . # # # .
+    # # # # #
+    . # # # .
+    . . # . .
+    . . # . .
+    # # # # #
+    # # # # #
+    . # # # .
+    `))
+makerbit.lcdMakeCharacter(LcdChar.c2, makerbit.lcdCharacterPixels(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `))
+makerbit.lcdMakeCharacter(LcdChar.c3, makerbit.lcdCharacterPixels(`
+    . . # . .
+    . # # # .
+    . # # # .
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    . # # # .
+    `))
+makerbit.lcdMakeCharacter(LcdChar.c4, makerbit.lcdCharacterPixels(`
+    . . . . .
+    . # # # .
+    # . # . #
+    # . # . #
+    # . . . #
+    . # # # .
+    . . . . .
+    . . . . .
+    `))
+makerbit.lcdMakeCharacter(LcdChar.c5, makerbit.lcdCharacterPixels(`
+    . . . . .
+    . . . # #
+    . . # . .
+    . # . . .
+    . # . . .
+    . # . . .
+    . . # . .
+    . . . # #
+    `))
+makerbit.lcdMakeCharacter(LcdChar.c6, makerbit.lcdCharacterPixels(`
+    . . . . .
+    # # # . .
+    # . . # .
+    # . . . #
+    # # # . .
+    . . . . #
+    . . . # .
+    # # # . .
+    `))
+loops.everyInterval(3000, function () {
     sensor_reading = pins.analogReadPin(AnalogPin.P0)
-    dryness = Math.map(sensor_reading, wet, dry, 0, 100)
-    OLED.clear()
-    OLED.writeStringNewLine("Emmett's watering kit")
-    OLED.writeString("Dryness: ")
-    OLED.writeNum(dryness)
-    OLED.writeStringNewLine(" / 100")
-    if (dryness >= dryness_trigger_level) {
-        OLED.writeStringNewLine("Watering...")
+    dryness = Math.map(sensor_reading, wet, dry, 1, 99)
+    wetness = 100 - Math.round(dryness)
+    makerbit.clearLcd1602()
+    makerbit.showStringOnLcd1602("Emmett's Waterer", makerbit.position1602(LcdPosition1602.Pos1), 16)
+    makerbit.showStringOnLcd1602("%", makerbit.position1602(LcdPosition1602.Pos19), 1)
+    makerbit.lcdShowCharacter1602(LcdChar.c3, makerbit.position1602(LcdPosition1602.Pos21))
+    makerbit.showStringOnLcd1602(convertToText(wetness), makerbit.position1602(LcdPosition1602.Pos17), 2)
+    if (wetness <= dryness_trigger_level) {
+        makerbit.showStringOnLcd1602("Water", makerbit.position1602(LcdPosition1602.Pos25), 6)
+        makerbit.lcdShowCharacter1602(LcdChar.c3, makerbit.position1602(LcdPosition1602.Pos31))
+        makerbit.lcdShowCharacter1602(LcdChar.c4, makerbit.position1602(LcdPosition1602.Pos32))
         pins.digitalWritePin(DigitalPin.P8, 1)
         basic.pause(10000)
         pins.digitalWritePin(DigitalPin.P8, 0)
-        OLED.writeStringNewLine("Waiting to soak...")
+        makerbit.showStringOnLcd1602("Soak  ", makerbit.position1602(LcdPosition1602.Pos25), 6)
+        makerbit.lcdShowCharacter1602(LcdChar.c5, makerbit.position1602(LcdPosition1602.Pos31))
+        makerbit.lcdShowCharacter1602(LcdChar.c6, makerbit.position1602(LcdPosition1602.Pos32))
         basic.pause(10000)
     } else {
         pins.digitalWritePin(DigitalPin.P8, 0)
-        OLED.writeStringNewLine("Soil is damp and healthy!")
+        makerbit.showStringOnLcd1602("Good!   ", makerbit.position1602(LcdPosition1602.Pos25), 6)
+        makerbit.lcdShowCharacter1602(LcdChar.c1, makerbit.position1602(LcdPosition1602.Pos31))
+        makerbit.lcdShowCharacter1602(LcdChar.c2, makerbit.position1602(LcdPosition1602.Pos32))
     }
-    
 })
